54dfc600336802464bd31c8f6ce5a725c9765339
zyy <l>
2019-09-09 21:19:29 +0800

    enhancement #1 完成pojo

---
+++ pojo/src/main/java/com/itszt/domian/Goods.java
@@ -0,0 +1,24 @@
+package com.itszt.domian;
+
+/**
+ * Created by zyy on 2019/9/9.
+ */
+public class Goods {
+
+    private String price;
+
+    public Goods() {
+    }
+
+    public Goods(String price) {
+        this.price = price;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
---
+++ pojo/src/main/java/com/itszt/inters/IGoodsServoce.java
@@ -0,0 +1,10 @@
+package com.itszt.inters;
+
+import com.itszt.domian.Goods;
+
+/**
+ * Created by zyy on 2019/9/9.
+ */
+public interface IGoodsServoce {
+    public Goods getGoods();
+}
